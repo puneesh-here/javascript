51d9ae68236bdea63bfce3b59995940a8ec16f3b
Puneesh-here <web.puneesh@gmail.com>
2021-08-04 21:25:55 +0530

    introduction to js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+let name = 'puneesh';
+console.log(name);
 
+let insertRate = 0.3;
+insertRate = 1;
+console.log(insertRate);
+
+let naam = 'puni';
+let age = '15';
+let isApproved = false;
+let firstName = undefined;
+let selectedColor = null;
+
+let person = {
+  firstname: 'punii',
+  age:23
+};
+
+//dot notation
+person.firstname = 'Puneesh is besst';
+
+//bracket notation
+person['firstname'] = 'harpik';
+
+console.log(person.firstname);
+
+
+let selectcolors = ['red', 'blue','green','pink'];
+console.log(selectcolors);
+
+let selectedcolors = ['red', 'blue','green','pink'];
+console.log(selectedcolors.length);
