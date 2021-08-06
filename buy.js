b2019312940e3524dffa462de44be8172ebab1a1
Puneesh-here <web.puneesh@gmail.com>
2021-08-06 08:49:46 +0530

    add and subtract numbers with JS

---
+++ buy.js
@@ -0,0 +1,20 @@
+document.getElementById(add);
+
+
+function addNumber(add){
+    document.getElementById(number);
+    var number = 5;number++;
+    document.write(number);
+    
+}
+
+
+document.getElementById(subtract);
+
+
+function subNumber(subtract){
+    document.getElementById(number);
+    var number = 5;number--;
+    document.write(number);
+    
+}
--- index.js
+++ index.js
@@ -30,3 +30,18 @@ console.log(selectcolors);
 
 let selectedcolors = ['red', 'blue','green','pink'];
 console.log(selectedcolors.length);
+
+var num1 = 10;
+var num2 = 20;
+if (num1 < num2) {
+  console.log("hii")
+}
+
+let mynum1 = 5;
+let mynum2 = 10;
+if (mynum1 > mynum2){
+  console.log("hi!");
+}
+else{
+  console.log("bye");
+}
